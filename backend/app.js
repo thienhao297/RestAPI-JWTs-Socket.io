897528979f7f47b03169303fba124bcce75c1775
Hào Nguyễn <118797121+thienhao297@users.noreply.github.com>
2023-10-06 09:48:15 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,6 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 8080;
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "images");
